48ec6ffb4c3af5fd12d24b9eb0add6e02bdd64e5
Vladosok96 <vmixoks@gmail.com>
2024-01-10 18:56:31 +0400

    added some styles

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 import main
 from gevent.pywsgi import WSGIServer
 
-http_server = WSGIServer(("127.0.0.1", 8080), main.app)
+http_server = WSGIServer(("0.0.0.0", 8080), main.app)
 http_server.serve_forever()
