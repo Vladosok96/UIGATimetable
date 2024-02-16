2db68754d7b59e1184c8f3686418b778085ec4b6
vladosok96 <vmixoks@gmail.com>
2024-02-16 12:27:03 +0400

    edit

--- wsgi.py
+++ wsgi.py
@@ -2,8 +2,8 @@ import main
 from gevent.pywsgi import WSGIServer
 
 
-# main.app.run(debug=True)
+main.app.run(debug=True)
 
 
-http_server = WSGIServer(("0.0.0.0", 8080), main.app)
-http_server.serve_forever()
+# http_server = WSGIServer(("0.0.0.0", 8080), main.app)
+# http_server.serve_forever()
